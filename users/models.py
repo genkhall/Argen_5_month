fd06c5d72a3465c583d20953250727fb55ac35cd
genkhall <argenargen0805@gmail.com>
2023-08-11 16:40:07 +0600

    hw5

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.crypto import get_random_string
+
+
+class ConfirmUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.code
